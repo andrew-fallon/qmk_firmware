9f94654783b1de7e7724a09a82181c0695345963
Andrew Fallon <ajf.15@icloud.com>
2022-11-28 00:08:15 -0500

    add num layer

--- keyboards/planck/keymaps/andrew-fallon/keymap.c
+++ keyboards/planck/keymaps/andrew-fallon/keymap.c
@@ -17,7 +17,11 @@
 #define HOME_A LGUI_T(KC_A)
 
 // thumb keys - left
-#define NAV_SP LT(NAV, KC_SPC)
+// #define NAV_SP LT(NAV, KC_SPC)
+
+// thumb keys
+#define MO_NAV MO(NAV)
+#define MO_NUM MO(NUM)
 
 // one-shot mods
 #define OS_LSFT OSM(MOD_LSFT)
@@ -29,48 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_W    , KC_L    , KC_Y    , KC_P    , KC_B    , XXXXXXX , XXXXXXX , KC_Z    , KC_F    , KC_O    , KC_U    , KC_QUOT ,
         HOME_C  , HOME_R  , HOME_S  , HOME_T  , KC_G    , XXXXXXX , XXXXXXX , KC_M    , HOME_N  , HOME_E  , HOME_I  , HOME_A  ,
         KC_Q    , KC_J    , KC_V    , KC_D    , KC_K    , XXXXXXX , XXXXXXX , KC_X    , KC_H    , KC_SLSH , KC_COMM , KC_DOT  ,
-        XXXXXXX , XXXXXXX , KC_ESC  , NAV_SP  , KC_TAB  , XXXXXXX , XXXXXXX , KC_ENT  , KC_BSPC , OS_LSFT , XXXXXXX , XXXXXXX 
+        XXXXXXX , XXXXXXX , XXXXXXX , MO_NAV  , KC_SPC  , XXXXXXX , XXXXXXX , OS_LSFT , MO_NUM  , XXXXXXX , XXXXXXX , XXXXXXX 
 ),
 
 // NAVIGATION LAYER
 [NAV] = LAYOUT_planck_grid(
-        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_UP   , XXXXXXX , XXXXXXX ,
-        KC_LGUI , KC_LALT , KC_LSFT , KC_LCTL , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_RIGHT, XXXXXXX ,
-        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
-        XXXXXXX , XXXXXXX , XXXXXXX , _______ , XXXXXXX , XXXXXXX , XXXXXXX , KC_ENT  , KC_BSPC , KC_LSFT , XXXXXXX , XXXXXXX 
+        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGUP , KC_HOME , KC_UP   , KC_END  , XXXXXXX ,
+        KC_LGUI , KC_LALT , KC_LSFT , KC_LCTL , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGDN , KC_LEFT , KC_DOWN , KC_RIGHT, XXXXXXX ,
+        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_DEL  , KC_BSPC , KC_TAB  , KC_ENT  , XXXXXXX ,
+        XXXXXXX , XXXXXXX , XXXXXXX , _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_ESC  , XXXXXXX , XXXXXXX 
 ),
 
-/*
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
-[_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+// NUMBER/SYMBOL LAYER
+[NUM] = LAYOUT_planck_grid(
+        KC_GRV  , KC_4    , KC_5    , KC_6    , KC_EQL  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , 
+        KC_SCLN , KC_1    , KC_2    , KC_3    , KC_MINS , XXXXXXX , XXXXXXX , XXXXXXX , KC_LCTL , KC_LSFT , KC_LALT , KC_LGUI ,
+        KC_BSLS , KC_7    , KC_8    , KC_9    , KC_LBRC , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , 
+        XXXXXXX , XXXXXXX , XXXXXXX , KC_0    , KC_RBRC , XXXXXXX , XXXXXXX , XXXXXXX , _______ , XXXXXXX , XXXXXXX , XXXXXXX 
 ),
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
-[_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
-),
-*/
-
 };
